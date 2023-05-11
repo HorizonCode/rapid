63be7aba604cc4363c3dc1ba8103f50683520f0b
HorizonCode <horizoncode88@gmail.com>
2023-05-11 08:42:45 +0200

    update test example

--- example/test.ts
+++ example/test.ts
@@ -2,12 +2,13 @@ import { Status } from "https://deno.land/std@0.186.0/http/http_status.ts";
 import { HTTPServer } from "../http_server.ts";
 
 const httpServer = new HTTPServer();
+
 httpServer.add("GET", "/", (req, rep) => {
   rep.status(Status.Teapot)
     .header("working", "true")
     .type("application/json")
     .cookie("working", "true");
-  
+
   console.log(req.cookie("working"));
 
   return JSON.stringify(
@@ -19,21 +20,24 @@ httpServer.add("GET", "/", (req, rep) => {
     2,
   );
 });
+
 httpServer.add("GET", "/api/user/:userId", (req, rep) => {
   rep.status(Status.Teapot)
-  .type("application/json");
+    .type("application/json");
 
   console.log(req.queryParams);
 
   return JSON.stringify(
     {
       code: Status.Teapot,
-      message: `UserID is ${req.pathParams["userId"]}`,
+      message: `UserID is ${req.pathParam("userId")}`,
+      queryParams: req.queryParams,
     },
     null,
     2,
   );
 });
+
 httpServer.listen({
   port: 8080,
   staticLocalDir: "/static",
